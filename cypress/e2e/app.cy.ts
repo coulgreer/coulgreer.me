ea2ff3501b0065924ee038f12f6d169c182a207c
Coul Greer <cagreer2016+github@gmail.com>
2023-05-29 16:59:52 -0400

    test: introduce tests for all major links

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -4,7 +4,7 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 
-  it("should navigate to the about page", () => {
+  it("should navigate to the recipe page", () => {
     cy.findByRole("link", { name: /cookbook/i }).click();
 
     const nav = cy.findByRole("navigation");
@@ -12,6 +12,42 @@ describe("Navigation", () => {
     const heading = cy.findByRole("heading", { name: /recipes/i });
 
     cy.url().should("include", "/recipes");
+
+    nav.should("exist");
+    footer.should("exist");
+    heading.should("exist");
+  });
+
+  it("should navigate to the blog page", () => {
+    cy.findByRole("link", { name: /journal/i }).click();
+
+    const nav = cy.findByRole("navigation");
+    const footer = cy.findByRole("contentinfo");
+    const heading = cy.findByRole("heading", { name: /blog/i });
+
+    nav.should("exist");
+    footer.should("exist");
+    heading.should("exist");
+  });
+
+  it("should navigate to the projects page", () => {
+    cy.findByRole("link", { name: /scrapbook/i }).click();
+
+    const nav = cy.findByRole("navigation");
+    const footer = cy.findByRole("contentinfo");
+    const heading = cy.findByRole("heading", { name: /projects/i });
+
+    nav.should("exist");
+    footer.should("exist");
+    heading.should("exist");
+  });
+
+  it("should navigate to the dictionary page", () => {
+    cy.findByRole("link", { name: /dictionary/i }).click();
+
+    const nav = cy.findByRole("navigation");
+    const footer = cy.findByRole("contentinfo");
+    const heading = cy.findByRole("heading", { name: /dictionary/i });
 
     nav.should("exist");
     footer.should("exist");
