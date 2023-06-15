e472c0e33c1c4b45789b381ef1ea62dc7892c21d
Coul Greer <cagreer2016+github@gmail.com>
2023-06-14 22:42:15 -0400

    chore: clean up styles
    Aim to test the styles against more realistic data on the dictionary
    page.

--- src/app/dictionary/page.tsx
+++ src/app/dictionary/page.tsx
@@ -18,25 +18,34 @@ export default function Dictionary() {
           <Typography variant="h2" className="word-entry__headword">
             cacophonous
           </Typography>
-          <p className="word-entry__pronunciation  ">/kəˈkɑː.fə.nəs/</p>
+          <p className="word-entry__pronunciation text-small">
+            /kəˈkɑː.fə.nəs/
+          </p>
           <hr className="word-entry__divider" />
           <p className="word-entry__part-of-speech">adjective</p>
         </div>
         <div className="word-entry__header-extension" />
         <div className="word-entry__body">
-          <p className="word-entry__definition">
-            having an unpleasant mixture of sounds
+          <p className="word-entry__definition text-size-h5">
+            having a harsh or discordant sound
           </p>
           <figure className="quote">
             <blockquote className="quote__text">
-              Here is a sentance that I heard or read that used cacophonous.
+              I was gonna say -- and I- I think this is a great thing -- I feel
+              like I&apos;ve noticed this trend where I think you lose your
+              patients more. The threshold is lower now at reunion. [...] But I
+              think it&apos;s a way to release the valve because you only ever
+              do it when it really gets so cacophonous that it&apos;s, like,
+              this has- we have to release this.
             </blockquote>
-            <figcaption className="quote__source">
-              <p className="quote__author">Speaker/Writer,</p>
+            <figcaption className="quote__source text-caption">
+              <p className="quote__author">Matt Rogers,</p>
               <p className="quote__creative-work">
-                <cite>The piece of work</cite>
+                <cite>Las Culturistas with Matt Rogers and Bowen Yang</cite>
               </p>
-              <p className="quote__context">Granular context</p>
+              <p className="quote__context">
+                &quot;Dishing and Kibbitzing&quot; (w/ Andy Cohen)
+              </p>
             </figcaption>
           </figure>
         </div>
