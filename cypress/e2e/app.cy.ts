4d25f0034750f2b5b97cc972d346e5ea5559eeba
Coul Greer <cagreer2016+github@gmail.com>
2023-09-20 12:06:26 -0400

    fix: eliminate url reading failure in e2e tests
    Move assertions to the very end to allow for the page to load and the
    url to update for the test.

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,19 +1,16 @@
 describe("Navigation", () => {
   beforeEach(() => {
     cy.visit("/");
-    /* TODO (Coul Greer): Look into optimization or cypress waiting for a full load before testing */
-    cy.wait(3000); // The page does not fully load thus wait is needed.
   });
 
   it("should navigate to the recipe page", () => {
     cy.findByRole("link", { name: /cookbook/i }).click();
 
-    cy.url().should("include", "/recipes");
-
     const nav = cy.findByRole("navigation");
     const footer = cy.findByRole("contentinfo");
     const heading = cy.findByRole("heading", { name: /recipes/i });
 
+    cy.url().should("include", "/recipes");
     nav.should("exist");
     footer.should("exist");
     heading.should("exist");
@@ -22,12 +19,11 @@ describe("Navigation", () => {
   it("should navigate to the blog page", () => {
     cy.findByRole("link", { name: /journal/i }).click();
 
-    cy.url().should("include", "/blog");
-
     const nav = cy.findByRole("navigation");
     const footer = cy.findByRole("contentinfo");
     const heading = cy.findByRole("heading", { name: /blog/i });
 
+    cy.url().should("include", "/blog");
     nav.should("exist");
     footer.should("exist");
     heading.should("exist");
@@ -36,12 +32,11 @@ describe("Navigation", () => {
   it("should navigate to the projects page", () => {
     cy.findByRole("link", { name: /scrapbook/i }).click();
 
-    cy.url().should("include", "/projects");
-
     const nav = cy.findByRole("navigation");
     const footer = cy.findByRole("contentinfo");
     const heading = cy.findByRole("heading", { name: /projects/i });
 
+    cy.url().should("include", "/projects");
     nav.should("exist");
     footer.should("exist");
     heading.should("exist");
@@ -50,11 +45,10 @@ describe("Navigation", () => {
   it("should navigate to the dictionary page", () => {
     cy.findByRole("link", { name: /dictionary/i }).click();
 
-    cy.url().should("include", "/dictionary");
-
     const nav = cy.findByRole("navigation");
     const footer = cy.findByRole("contentinfo");
 
+    cy.url().should("include", "/dictionary");
     nav.should("exist");
     footer.should("exist");
   });
